32fbbb66e0e6d1e1acb649d2bb7705ef1a132182
tayasarbhat <tayasarbhat@gmail.com>
2024-12-18 16:52:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/finance-tracker/',
+  base: '/fintracker/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
